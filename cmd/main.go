c5c9533066ad4cb41b34d1365fadd84b0b3e9375
CoffeManFP <coffemanfp@gmail.com>
2020-07-31 19:59:13 -0400

    Added recommendations handler

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/coffemanfp/meander"
 )
@@ -22,6 +24,48 @@ func main() {
 
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
+	})
+
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		journey := strings.Split(r.URL.Query().Get("journey"), "|")
+		if len(journey) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("no journey data found."))
+			return
+		}
+
+		q := &meander.Query{
+			Journey: journey,
+		}
+
+		var err error
+
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("invalid latitude"))
+			return
+		}
+
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("invalid longitude"))
+			return
+		}
+
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("invalid radius"))
+			return
+		}
+
+		q.CostRangeStr = r.URL.Query().Get("cost")
+
+		places := q.Run()
+
+		respond(w, r, places)
 	})
 
 	http.ListenAndServe(":8080", http.DefaultServeMux)
