311ffff683b7d822b8a1564d59dbd9033bbdeec8
CoffeManFP <coffemanfp@gmail.com>
2020-07-31 19:26:15 -0400

    Created Google query struct and request

--- query.go
+++ query.go
@@ -1,7 +1,49 @@
 package meander
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
 // APIKey Google API Key
 var APIKey string
+
+type Query struct {
+	Lat          float64
+	Lng          float64
+	Journey      []string
+	Radius       int
+	CostRangeStr string
+}
+
+func (q *Query) find(types string) (gR *googleResponse, err error) {
+	urlString := "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+	vals := make(url.Values)
+
+	vals.Set("location", fmt.Sprintf("%g,%g", q.Lat, q.Lng))
+	vals.Set("radius", fmt.Sprintf("%d", q.Radius))
+	vals.Set("types", types)
+	vals.Set("key", APIKey)
+
+	if q.CostRangeStr == "" {
+		r := ParseCostRange(q.CostRangeStr)
+
+		vals.Set("minprice", fmt.Sprintf("%d", int(r.From)-1))
+		vals.Set("maxprice", fmt.Sprintf("%d", int(r.To)-1))
+	}
+
+	res, err := http.Get(urlString + "?" + vals.Encode())
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(&gR)
+	return
+}
 
 // Place is the Google Place model representation.
 type Place struct {
