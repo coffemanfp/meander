9f626e343b805e23a56d46888780fd6ca5901658
CoffeManFP <coffemanfp@gmail.com>
2020-07-30 20:54:44 -0400

    Created Google Place model

---
+++ query.go
@@ -0,0 +1,40 @@
+package meander
+
+// Place is the Google Place model representation.
+type Place struct {
+	*googleGeometry `json:"geometry"`
+	Name            string         `json:"name"`
+	Icon            string         `json:"icon"`
+	Photos          []*googlePhoto `json:"photos"`
+	Vicinity        string         `json:"vicinity"`
+}
+
+type googleResponse struct {
+	Results []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	*googleLocation `json:"location"`
+}
+
+type googleLocation struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhoto struct {
+	PhotoRef string `json:"photoRef"`
+	URL      string `json:"url"`
+}
+
+// Public returns the public version of Place.
+func (p *Place) Public() interface{} {
+	return map[string]interface{}{
+		"name":     p.Name,
+		"icon":     p.Icon,
+		"photos":   p.Photos,
+		"vicinity": p.Vicinity,
+		"lat":      p.Lat,
+		"lng":      p.Lng,
+	}
+}
